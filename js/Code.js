941b88281f7572caefd21ae9ce262c7b409c71b9
YugAngrish <75120412+YugAngrish@users.noreply.github.com>
2021-05-15 20:42:23 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+fill("white")
+textSize(15)
+text("C F N U N O I T", 700,150)
+fill("lightblue")
+text("Hint: Performs a particular task !!", 700,170)
+
+fill("white")
+textSize(15)
+text("S D T A A B A E", 100,450)
+fill("lightblue")
+text("Hint: Place where everything is Stored!!", 100,470)
+
+}
